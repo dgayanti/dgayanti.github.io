f6f158cd290448a96ef1aaec99ada8629c0cfe6e
irenkamalova <irenkamalova@yandex-team.ru>
2018-09-16 12:58:49 +0300

    old files

---
+++ b.js
@@ -0,0 +1,54 @@
+document.write('\
+\
+\
+<div id="includedContent"></div>\
+   	<div class="container" style="margin-top: 0px">\
+\
+\
+			<div class="row">\
+			<nav class="navbar navbar-default ">\
+			<div class="navbar-collapse collapse ">\
+			<ul class="nav navbar-nav ">\
+					<li class="dropdown">\
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">\
+							О нас<b class="caret"></b></a>\
+						<ul class="dropdown-menu">\
+						<li><a href="services.html">История студии</a></li>\
+						<li class="divider"></li>\
+						<li><a href="schools.html">Наши направления</a></li></ul>\
+					</li>\
+					<li><a href="preps.html">Наши преподаватели</a></li>\
+					<li><a href="photogallery.html">Фото галерея</a></li>\
+					<li><a href="videogallery.html">Видео галерея</a></li>\
+					</ul>\
+					 <div class="navbar-default navbar-right">\
+				  		\
+				  		<a href="#" class="navbar-text" data-toggle="modal" data-target="#myModal">Вход</a>\
+				  	\
+				  	</div>\
+				</div>\
+			</div>\
+			</nav>\
+\
+\<!-- Modal -->\
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">\
+  <div class="modal-dialog" role="document">\
+    <div class="modal-content">\
+      <div class="modal-header">\
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>\
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>\
+      </div>\
+      <div class="modal-body">\
+        <img src="./bootstrap-prestructure_files/555x300">\
+        PamPamPam\
+      </div>\
+      <div class="modal-footer">\
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>\
+        <button type="button" class="btn btn-primary">Save changes</button>\
+      </div>\
+    </div>\
+  </div>\
+</div>\
+\
+\
+');
